c23b7c36ea60319ccc58e51ddc9d3ad08fc8d8e4
Jeremy <jeremyeparker@yahoo.com>
2022-05-31 13:35:04 -0400

    spring batch

---
+++ batch-process/src/test/java/com/atsignjar/batch/process/BatchProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atsignjar.batch.process;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
