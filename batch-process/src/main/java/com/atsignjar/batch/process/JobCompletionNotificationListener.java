b705d1c41a2bc5f8f7ea50e591a391d1eb09f813
Jeremy <jeremyeparker@yahoo.com>
2022-06-04 14:47:25 -0400

    notifications

--- batch-process/src/main/java/com/atsignjar/batch/process/JobCompletionNotificationListener.java
+++ batch-process/src/main/java/com/atsignjar/batch/process/JobCompletionNotificationListener.java
@@ -1,2 +1,36 @@
-package com.atsignjar.batch.process;public class JobCompletionNotificationListener {
+package com.atsignjar.batch.process;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
+
+    private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
+
+    private final JdbcTemplate jbdcTemplate;
+
+    @Autowired
+    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate){
+        this.jbdcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        if(jobExecution.getStatus() == BatchStatus.COMPLETED){
+            log.info("!!! Job Complete! Please verify the results");
+
+            jbdcTemplate.query("SELECT first_name, last_name FROM people",
+                    (rs,row) -> new Person(
+                            rs.getString(1),
+                            rs.getString(2))
+                    ).forEach(person -> log.info("Found <" + person + "> in the database."));
+        }
+    }
 }
