71faf04835f64b372dc5d19947b8177c898e28da
Jeremy <jeremyeparker@yahoo.com>
2022-06-04 14:46:35 -0400

    ensure JVM exits when job completes

--- batch-process/src/main/java/com/atsignjar/batch/process/BatchProcessApplication.java
+++ batch-process/src/main/java/com/atsignjar/batch/process/BatchProcessApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BatchProcessApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BatchProcessApplication.class, args);
+	public static void main(String[] args)throws Exception {
+
+	System.exit(SpringApplication.exit(SpringApplication.run(BatchProcessApplication.class, args)));
 	}
 
 }
